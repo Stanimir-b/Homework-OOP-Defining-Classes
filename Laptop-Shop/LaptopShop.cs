680cba8ac91ba3bb47909d85e5fcad5a243b6f01
Stanimir-b <stanimir.b@gmail.com>
2015-06-04 11:58:12 +0300

    Create LaptopShop.cs

---
+++ Laptop-Shop/LaptopShop.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+class LaptopShop
+{
+    static void Main()
+    {
+
+
+        Laptop laptop = new Laptop("Lenovo", 2259.00m, new Battery());//Battery is optional.
+        //The following are optional.
+        laptop.Manufacturer = "Lenovo";
+        laptop.Processor = "Intel Core i5-4210U (2-core, 1.70 - 2.70 GHz, 3MB cache)";
+        laptop.Ram = 8;
+        laptop.GraphicsCard = "Intel HD Graphics 4400";
+        laptop.Hdd = 128;
+        laptop.Screen = @"13.3"" (33.78 cm) – 3200 x 1800 (QHD+), IPS sensor display";
+       
+        Console.WriteLine(laptop);
+    }
+}
