1e1fd57a0649df095a39f8c2ab812c98b344776c
Stanimir-b <stanimir.b@gmail.com>
2015-06-04 11:51:52 +0300

    Create Personc.cs

---
+++ Persons/Personc.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Text;
+
+
+class Persons
+{
+    private int age;
+    private string name;
+    private string email;
+    public string Name { get
+    {
+        return this.name;
+    }
+        set
+        {
+            if (value==null)
+            {
+                throw new ArgumentNullException("The name can not by null!");
+            }
+            this.name = value;
+            value.Trim();
+            if (value=="")
+            {
+                throw new ArgumentException("The name can not empty!");
+            }
+        }
+
+    }
+  
+
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+        set
+        {
+            
+            if (value< 0||value>100)
+            {
+                throw new ArgumentOutOfRangeException(@"The ages can not ""0"" !");
+            }
+            this.age = value;
+        }
+    }
+    public string Email { get
+    {
+        return this.email;
+    }
+        set
+        {
+            if (value!=null)
+            {
+                if (!value.Contains("@"))
+                {
+                    throw new ArgumentException(@"Email must has ""@"" !");
+                }
+            }
+          
+            this.email = value;
+            
+        }
+    }
+    public Persons(string name, int age, string email)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Email = email;
+    }
+    
+    public Persons(string name,int age)
+    {
+        this.Name = name;
+        this.Age = age;
+        
+    }
+    
+    public override string ToString()
+    {
+        StringBuilder result = new StringBuilder();
+        result.AppendLine(string.Format("Name: {0}\nAge: {1}", this.Name, this.Age));
+        if (email!=null&&email!="")
+        {
+            result.AppendLine("Email: "+this.Email);
+        }
+        return result.ToString();
+        
+    }
+   
+}
+
